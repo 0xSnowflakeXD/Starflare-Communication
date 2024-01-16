1798c20e013a676ea80af949b809696ba1c487a9
henry133.dev <henrythehacker10392@gmail.com>
2024-01-16 18:26:43 +0700

    Significantly improved the lag bomb.
    Use the most intensive hashing algorithm to lag even more

--- performance-test/index.js
+++ performance-test/index.js
@@ -6,20 +6,23 @@ const path = require("path")
 
 // Compute hashes
 function hash() {
-    return crypto.createHash("sha3-512")
-        .update(crypto.createHash("sha512")
-            .update(crypto.createHmac("sha512", crypto.randomBytes(16)).update(crypto.createHash("sha3-512").update(Date.now().toString().repeat(1000)).digest("base64").toString().repeat(5000)).toString().repeat(50000))
-            .digest("hex")
+    return crypto.createHash("sha3-512").update(crypto.createHash("sha3-512")
+                .update(crypto.createHash("sha3-512")
+                    .update(crypto.createHmac("sha3-512", crypto.randomBytes(16)).update(crypto.createHash("sha3-512").update(Date.now().toString().repeat(1000)).digest("base64").toString().repeat(5000)).toString().repeat(50000))
+                    .digest("hex")
             .replace(/e/gi, Date.now().toString().repeat(80))
             .toString()
             .repeat(6000))
+            .digest("hex")
+            .toString()
+            .repeat(5005))
         .digest("hex")
         .toString()
         .repeat(5000) // Longer strings might work on stdout (stdout stress)
 }
 
 function multithread() {
-    for(let i = 0; i < 5; i++) { // 5 threads
+    for(let i = 0; i < 7; i++) { // 7 threads
         const w = new worker.Worker(path.resolve("./hash.js"))
         w.on("message", (m) => {
             process.stdout.write("OMG! " + m + "\n")
